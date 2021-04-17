76ec4ea88c617a88b33bdf33fbcf3747cc2a3347
va6un <va6un.b@gmail.com>
2021-04-17 20:03:16 +0530

    modify

--- index.js
+++ index.js
@@ -9,21 +9,25 @@ app.use(express.static('public'));
 
 app.get('/api', async (req, res) => {
     const districts = ['Trivandrum', 'Kollam', 'Alappuzha', 'Pathanamthitta', 'Kottayam', 'Idukki', 'Ernakulam', 'Thrissur', 'Palakkad', 'Malappuram', 'Kozhikode', 'Kalpatta', 'Kannur', 'Kasaragod'];
-
     const key = process.env.API_KEY;
-
-
     const weather = [];
 
     for (district of districts) {
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${district}&appid=${key}&units=metric`;
-        const response = await fetch(url);
+        const response = await fetch(url).then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                throw new Error('Response from fetch: ', response.ok);
+            }
+        }).catch(error => {
+            console.error(error);
+            res.end();
+        });
         const data = await response.json();
         weather.push(data);
+
     }
-
-    // console.log(weather);
-
     res.json(weather);
 
 });
--- public/script.js
+++ public/script.js
@@ -1,10 +1,11 @@
-const map = L.map('map', { zoomControl: false }).setView([10.519475572241758, 76.21569782786551], 4);
+/**
+ * handle fetch error: https://stackoverflow.com/questions/38235715/fetch-reject-promise-and-catch-the-error-if-status-is-not-ok
+ */
+const card = document.getElementById('card');
+card.style.display = 'none';
+const map = L.map('map', { zoomControl: false }).setView([8.4833, 76.9167], 9);
 const mapTileURL = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}';
 const mapTileAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
-const startBtn = document.getElementById('startBtn');
-const card = document.getElementById('card');
-
-card.style.display = 'none';
 
 L.tileLayer(mapTileURL, {
     attribution: mapTileAttribution,
@@ -15,10 +16,6 @@ L.tileLayer(mapTileURL, {
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoidmFydW5iIiwiYSI6ImNrbmZqYnQwMDJ2ZTUycXA5Y2Zya2QzM3gifQ.r7iv0_XbuD2Y8fzN0BmY8A'
 }).addTo(map);
-
-const delay = (time) => {
-    return new Promise(resolve => setTimeout(resolve, time));
-}
 
 const getTodaysDate = (milli) => {
     const date = new Date(milli);
@@ -31,47 +28,57 @@ const getTodaysDate = (milli) => {
     return `${weekDays[day]} ${time}:${minutes} ${ampm}`;
 }
 const updateUI = (district) => {
-    const name = document.getElementById('name');
+    const city = document.getElementById('city');
     const date = document.getElementById('date');
     const description = document.getElementById('description');
+    const icon = document.getElementById('icon');
     const temperature = document.getElementById('temperature');
     const feels = document.getElementById('feels');
     const humidity = document.getElementById('humidity');
     const wind = document.getElementById('wind');
     const milli = new Date();
 
-    name.textContent = district.name;
+    city.textContent = district.name;
     date.textContent = getTodaysDate(milli);
     description.textContent = district.weather[0].description;
+    icon.src = `/icons/${district.weather[0].icon}@2x.png`;
     temperature.textContent = parseInt(district.main.temp);
     feels.textContent = parseInt(district.main.feels_like);
     humidity.textContent = district.main.humidity;
     wind.textContent = district.wind.speed;
 }
 
-const startUpdating = async () => {
+const letsFly = async () => {
     const response = await fetch('/api');
-    const state = await response.json();
-    // console.log(state);
-    const marker = L.marker();
-    // const popup = L.popup();
 
-    for (district of state) {
-        await delay(3000);
-        // console.log(district);
-        await map.setView([district.coord.lat, district.coord.lon], 9, {
-            "animate": true,
-            "pan": {
-                "duration": 2
-            }
-        });
-        document.getElementById('loader').style.display = 'none';
-        // popup.setLatLng([district.coord.lat, district.coord.lon]).setContent('Hai');
-        marker.setLatLng([district.coord.lat, district.coord.lon]).addTo(map);
-        updateUI(district);
-        card.style.display = 'block';
+    // only in a true case we need to proceed further.
+    if (response.ok) {
+        return response;
+    } else {
+        throw new Error('Response from fetch request: ', response.ok);
     }
 }
 
-startUpdating();
-        // startBtn.addEventListener('click', startUpdating);
+const sleep = time => new Promise(resolve => setTimeout(resolve, time));
+
+letsFly().then(async response => {
+    const state = await response.json();
+    const marker = L.marker();
+
+    for (district of state) {
+        // console.log(district);
+        document.getElementById('overlay').style.display = 'none';
+        map.setView([district.coord.lat, district.coord.lon], 9, {
+            "animate": true,
+            "pan": {
+                "duration": 3
+            }
+        });
+        marker.setLatLng([district.coord.lat, district.coord.lon]).addTo(map);
+        updateUI(district);
+        card.style.display = 'block';
+        await sleep(4000);
+    }
+}).catch(error => {
+    console.log(error)
+});
